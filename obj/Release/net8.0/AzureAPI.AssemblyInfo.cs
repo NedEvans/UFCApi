6cdb3542d2fe662fdc8743fd64df429aa3d0347c
NedEvans <hrh.neilevans@gmail.com>
2025-09-12 18:21:34 +0800

    Working on port 8080

--- obj/Release/net8.0/AzureAPI.AssemblyInfo.cs
+++ obj/Release/net8.0/AzureAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AzureAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f312dcc3e28b084b0d27bf444f5e1cf9c215172")]
 [assembly: System.Reflection.AssemblyProductAttribute("AzureAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AzureAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
